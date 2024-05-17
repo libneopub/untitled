0da13d12c42112962f5a14696cfb28e5df242fed
Robin Boers <robindev2019@outlook.com>
2024-05-17 22:15:39 +0200

    Don't show settings button on login screen

--- cms/partials/header.php
+++ cms/partials/header.php
@@ -1,4 +1,7 @@
 <header>
   <h1><a href="<?= CANONICAL.CMS ?>">Pebble</a></h1>
-  <a href="<?= CANONICAL.CMS . "/settings" ?>">Settings</a>
+
+  <?php if(isset($_SESSION['access_token'])) { ?>
+    <a href="<?= CANONICAL.CMS . "/settings" ?>">Settings</a>
+  <?php } ?>
 </header>
